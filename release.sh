359686c7b67d2949d6a2ba23876595b0b41aef60
Ryan Doherty <tech@conical.org>
2022-04-20 00:29:54 -0400

    Add release script

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$GITHUB_USERNAME" == "" ]; then
+    echo 'Environment variable $GITHUB_USERNAME must be set'
+    exit 1
+fi
+if [ "$GITHUB_TOKEN" == "" ]; then
+    echo 'Environment variable $GITHUB_TOKEN must be set'
+    exit 1
+fi
+echo "Github credentials for $GITHUB_USERNAME found in environment"
+
+echo "Fetching settings.xml" \
+     && wget https://raw.githubusercontent.com/VEuPathDB/base-pom/main/settings.xml \
+     && echo "Preparing release..." \
+     && mvn -Dusername=git release:prepare \
+     && echo "Prepare successful; deploying artifacts" \
+     && mvn release:perform --settings ./settings.xml \
+     && echo "Release succeeded. Cleaning up." \
+     && git push
+
+echo "Done."

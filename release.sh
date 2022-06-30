37c7168c68beb738334c8428e21f507888821dfd
Ryan Doherty <tech@conical.org>
2022-06-30 16:05:13 -0400

    Use script and settings.xml in new maven-release-tools repo

--- release.sh
+++ release.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-if [ "$GITHUB_USERNAME" == "" ]; then
-    echo 'Environment variable $GITHUB_USERNAME must be set'
-    exit 1
-fi
-if [ "$GITHUB_TOKEN" == "" ]; then
-    echo 'Environment variable $GITHUB_TOKEN must be set'
-    exit 1
-fi
-echo "Github credentials for $GITHUB_USERNAME found in environment"
-
-echo "Fetching settings.xml" \
-     && curl -O https://raw.githubusercontent.com/VEuPathDB/base-pom/main/settings.xml \
-     && echo "Preparing release..." \
-     && mvn -Dusername=git release:prepare \
-     && echo "Prepare successful; deploying artifacts" \
-     && mvn release:perform --settings ./settings.xml \
-     && echo "Release succeeded. Cleaning up." \
-     && git push
-
-echo "Done."
+curl https://raw.githubusercontent.com/VEuPathDB/maven-release-tools/main/release-project.sh | bash
